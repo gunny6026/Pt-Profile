9f624f9b1cc22ddbec63430c1b97c03f376f9d84
gunny6026 <gunny6026@naver.com>
2021-01-04 18:33:48 +0900

    ResponseEntity

--- src/main/java/com/cos/jwt/web/UserController.java
+++ src/main/java/com/cos/jwt/web/UserController.java
@@ -8,6 +8,9 @@ import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -24,56 +27,96 @@ import com.cos.jwt.domain.user.User;
 import com.cos.jwt.domain.user.UserRepository;
 import com.cos.jwt.service.UserService;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @RestController
 public class UserController {
 
-	@Autowired // 의존성 주입 DI
-	private UserRepository userRepository;
 	
-	@Autowired
-	private  HttpSession session;
-	
-	@Autowired
-	private UserService userService;
+	private final UserRepository userRepository;
+	private final HttpSession session;
+	private final UserService userService;
 
 	
 	
 	//회원가입
-	@PostMapping("/join")
-	public ResponseEntity<?> join(@RequestBody User object) {
+	@PostMapping("/user/join")
+	public ResponseEntity<?> join(@RequestBody User user) {
 		
-		System.out.println(object);
+		System.out.println(user);
 	//	object.setAuth_pt(true);
-		userService.join(object);
+		userService.join(user);
 		//userRepository.save(object);
 		
 		return new ResponseEntity<String>("ok",HttpStatus.CREATED);
 	}
+	@GetMapping("/user/info") // 유저 개인정보
+	public ResponseEntity<?> userInfo(){
+		User user = (User) session.getAttribute("principal");
+		User userEntity = useService.info(user);
+		
+		return new ResponseEntity<User>(userEntity, HttpStatus.OK);
+	}
+	@PutMapping("/user/modify") //유저 수정
+	public ResponseEntity<?> modify(@RequestBody UserDTO userDto){
+		User userEntity = (User) session.getAttribute("principal");
+		System.out.println(userDto);
+		userService.modify(userDto, userEntity);
+		
+		return new ResponseEntity<String>("ok", HttpStatus.OK);
+		
+	}
+	@DeleteMapping("/user.remove") //유저 탈퇴
+	public ResponseEntity<?> remove(){
+		User user = (User) session.getAttribute("principal");
+		System.out.println(user);
+		int result = userService.remove(user);
+		if(result == 1) {
+			return new ResponseEntity<String>("ok" , HttpStatus.OK);
+			
+		}else {
+			return new ResponseEntity<String>("fail", HttpStatus.FORBIDDEN);
+		}
+	}
+	
+	@GetMapping("/admin/userList") // 유저 목록
+	public ResponseEntity<?> userList(@PageableDefault(sort ="userNo" , direction = Direction.DESC , size = 15) Pageable page){
+		User admin = (User) session.getAttribute("principal");
+		return new ResponseEntity<Page<User>>(userService.userList(admin,page), HttpStatus.OK);
+	}
 	
 	//로그 아웃
-	@GetMapping("/logout")
+	@GetMapping("/user/logout")
 	public ResponseEntity<?> logout(){
 		session.invalidate();
 		
 		return new ResponseEntity<String>("ok", HttpStatus.OK);
 	}
 	
-	//DB에서 아이디 값 가져오기 에러 패턴 익히기
-	@GetMapping("/login/{user_no}")
-	public User select(@PathVariable int user_no) {
-
-		User user = userRepository.findById(user_no).orElseThrow(new Supplier<IllegalArgumentException>() {
-
-			@Override
-			public IllegalArgumentException get() {
-				// TODO Auto-generated method stub
-				return new IllegalArgumentException("해당 사용자 없음");
-			}
-		});
-
-		return user;
+	@PutMapping("/admin/userAuth/{no}")
+	public ResponseEntity<?> userAuth(@PathVariable int no){
+		System.out.println(no);
+		userService.userAuth(no);
+		return new ResponseEntity<String>("ok" , HttpStatus.OK);
 	}
 	
+//	//DB에서 아이디 값 가져오기 에러 패턴 익히기
+//	@GetMapping("/login/{user_no}")
+//	public User select(@PathVariable int user_no) {
+//
+//		User user = userRepository.findById(user_no).orElseThrow(new Supplier<IllegalArgumentException>() {
+//
+//			@Override
+//			public IllegalArgumentException get() {
+//				// TODO Auto-generated method stub
+//				return new IllegalArgumentException("해당 사용자 없음");
+//			}
+//		});
+//
+//		return user;
+//	}
+//	
 	//로그인하기
 	//@GetMapping("loginex/{user_no}")
 	//public String login(@PathVariable int user_no) {
@@ -94,59 +137,59 @@ public class UserController {
 	
 	
 	//빌더 패던 회원가입 연습
-	@PostMapping("/home")
-	public String home() {
-
-		User user = User.builder().address("강남").email("rjslek@naver.com").auth_pt(true)
-				.password("ㅇㅇㅇㅇㅇㅇㅇㄴㄴ").name("거니찡").id("qkrrjsgml").build();
-
-		userRepository.save(user);
-
-		return "home: " + user;
-	}
+//	@PostMapping("/home")
+//	public String home() {
+//
+//		User user = User.builder().address("강남").email("rjslek@naver.com").auth_pt(true)
+//				.password("ㅇㅇㅇㅇㅇㅇㅇㄴㄴ").name("거니찡").id("qkrrjsgml").build();
+//
+//		userRepository.save(user);
+//
+//		return "home: " + user;
+//	}
 
 	// User에 있는 값 다 들고오기
-	@GetMapping("/list")
-	public List<User> userList(){
-		
-		 
-		
-		return userRepository.findAll();
-		
-	}
+//	@GetMapping("/list")
+//	public List<User> userList(){
+//		
+//		 
+//		
+//		return userRepository.findAll();
+//		
+//	}
 	
-	@Transactional
-	@PutMapping("/user/{user_no}")
-	public String userUpdate(@PathVariable int user_no , @RequestBody User user) {
-		
-		User DBuser = userRepository.findById(user_no).orElseThrow(() -> {
-			return new IllegalArgumentException("수정 실패");
-		}
-				);
-		DBuser.setId(user.getId());
-		DBuser.setName(user.getName());
-		DBuser.setPassword(user.getPassword());
-		DBuser.setEmail(user.getEmail());
-		
-		//userRepository.save(DBuser);
-		
-		return "수정완료"+DBuser;
-		
-	}
+//	@Transactional
+//	@PutMapping("/user/{user_no}")
+//	public String userUpdate(@PathVariable int user_no , @RequestBody User user) {
+//		
+//		User DBuser = userRepository.findById(user_no).orElseThrow(() -> {
+//			return new IllegalArgumentException("수정 실패");
+//		}
+//				);
+//		DBuser.setId(user.getId());
+//		DBuser.setName(user.getName());
+//		DBuser.setPassword(user.getPassword());
+//		DBuser.setEmail(user.getEmail());
+//		
+//		//userRepository.save(DBuser);
+//		
+//		return "수정완료"+DBuser;
+//		
+//	}
 	
-	@DeleteMapping("/user/{user_no}")
-	public String userDelete(@PathVariable int user_no) {
-	
-		
-		try {
-		userRepository.deleteById(user_no);
-		
-		}catch(EmptyResultDataAccessException e){
-			
-			return "해당 아이디가 없습니다";
-			
-		}
-		return "삭제완료 삭제된 no :"+user_no;
-		
-	}
+//	@DeleteMapping("/user/{user_no}")
+//	public String userDelete(@PathVariable int user_no) {
+//	
+//		
+//		try {
+//		userRepository.deleteById(user_no);
+//		
+//		}catch(EmptyResultDataAccessException e){
+//			
+//			return "해당 아이디가 없습니다";
+//			
+//		}
+//		return "삭제완료 삭제된 no :"+user_no;
+//		
+//	}
 }
